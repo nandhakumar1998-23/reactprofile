c9376af3c21cad1c4df966ef65745f35eeca3a76
Nandhakumar <nandhakumarid023@gmail.com>
2024-10-30 21:17:24 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,139 @@
+const logotext = "Nandha_Resume";
+const meta = {
+    title: "Nandharesume",
+    description: "I’m Nandhakumar data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Nandhakumar",
+    animated: {
+        first: "Web Designer",
+        second: "Web Application Developer",
+        third: "Project Management",
+    },
+    description: "Experienced Web Designer and Front-End Developer specializing in responsive, user-friendly interfaces with expertise in UI | UX, HTML, Css, Js, Bootstrap, ReactJS, React Native, Tailwind CSS integration.",
+    your_img_url: "https://file.aiquickdraw.com/imgcompressed/img/compressed_a0698d76179ca0e6b38e1dfc7f1a458c.webp",
+};
+
+const dataabout = {
+    title: "My self",
+    aboutme: "Web Designer and Front-End Developer with 3+ years of experience specializing in ReactJS & React Native, Tailwind CSS, and creating responsive websites and applications using HTML, CSS, JavaScript, Bootstrap, and backend frameworks such as Laravel and Django. Skilled in building user-friendly interfaces, enhancing user experiences, and integrating back-end support. Proven track record of developing projects from scratch, collaborating in team environments, and contributing to open-source projects. Successfully worked with various clients as a freelancer and in collaborative settings to deliver high-quality, performance-optimized solutions.",
+};
+const worktimeline = [{
+        jobtitle: "Led a team to develop front-end applications using Bootstrap, ReactJS, and React Native, while supporting back-end processes with Django. Worked on projects like Violet Infotech, Zebra Sign World, Genesis, and Farm, integrating Electron.js to build cross-platform desktop applications and enhance user accessibility and performance. Developed a chatbot using Dialogflow to streamline client interactions and provide real-time assistance. Emphasized creating user-friendly UI/UX designs, ensuring seamless navigation and positive user experiences. Delivered client-centric solutions tailored to project requirements, consistently meeting deadlines and exceeding expectations. Contributed to the development of a precise billing ERP, leveraging Electron.js for an efficient and user-friendly interface.",
+        where: "Violet Infotech",
+        date: "2024",
+    },
+    {
+        jobtitle: "Worked as a Front-End Developer using React and Laravel to build responsive, dynamic interfaces for projects like AshHotel, AshRooms, and Pavico, ensuring smooth and user-friendly experiences across all devices. Collaborated with back-end teams to implement seamless API integrations, consistently delivering high-quality projects on time. Supported SEO initiatives and Google marketing strategies, increasing search rankings and driving online visibility. Contributed to social media campaigns and video traffic initiatives, enhancing audience engagement and expanding brand reach. Actively participated in cross-functional team meetings to align project goals, improve workflows, and ensure client satisfaction. Recognized for strong problem-solving skills and attention to detail in every project, with a focus on client-centric solutions.",
+        where: "Thiran Infotech",
+        date: "2023",
+    },
+    {
+        jobtitle: "Operated as an XML/HTML Operator in a startup environment, managing data-driven content and transforming it for web applications. Gained valuable experience in handling XML data structures, ensuring efficient data management and presentation. Contributed to the development of a Bible book application, utilizing XML for organizing content and .NET for implementing simple yet effective design elements. Ensured the application was user-friendly and intuitive, allowing users to navigate easily through various books and chapters. Collaborated with cross-functional teams to refine functionalities and enhance the overall user experience. Applied best practices for coding and data organization to optimize performance and loading speeds. Engaged in troubleshooting and debugging processes to maintain data integrity. Although the company closed after 9 months, the experience significantly enriched my skill set in data transformation and application development.",
+        where: "Silver Doller Technology",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "UI | UX",
+        value: 100,
+    },
+    {
+        name: "HTML, CSS, JS, SEO",
+        value: 100,
+    },
+    {
+        name: "Bootstrap, Tailwindcss",
+        value: 100,
+    },
+    {
+        name: "Electronjs, Reactjs, Nextjs, Basic React Native",
+        value: 96,
+    },
+    {
+        name: "Team Guid",
+        value: 97,
+    },
+];
+
+const services = [
+    {
+        title: "Website Development",
+        description: "Development of responsive and high-performance websites.\n" +
+                     "Utilizing modern frameworks to ensure a seamless user experience.\n" +
+                     "Focus on SEO best practices to enhance online visibility.",
+    },
+    {
+        title: "Application Development",
+        description: "Creating user-friendly mobile and web applications.\n" +
+                     "Implementing intuitive interfaces for better engagement.\n" +
+                     "Ensuring cross-platform compatibility for wider reach.",
+    },
+    {
+        title: "Desktop Development",
+        description: "Building robust desktop applications for various platforms.\n" +
+                     "Optimizing performance for efficient resource use.\n" +
+                     "Incorporating user feedback to enhance functionality.",
+    },
+];
+
+
+const dataportfolio = [
+    {
+        img: "https://cdn.dribbble.com/userupload/15054008/file/original-141bb779e7d375b20028861042e1fcd4.png?resize=400x300&vertical=center",
+        description: "Zebra Sign World",
+        link: "https://www.zebrasignworld.com/",
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2023/12/16/21/18/ai-generated-8453280_1280.jpg",
+        description: "Violet Infotech",
+        link: "https://www.violetinfotech.in/",
+    },
+    {
+        img: "https://html.design/wp-content/uploads/2019/12/Keto-600x800.png",
+        description: "Ashroomz",
+        link: "https://ashroomz.in/",
+    },
+    {
+        img: "https://pbs.twimg.com/media/GToVznTW8AAgpgF.jpg",
+        description: "Ash Hotels",
+        link: "https://www.ashhotels.in/",
+    },
+    {
+        img: "https://pavicouae.com/public/uploads/project-featured-photo-14.jpg",
+        description: "Pavi Co. UAE",
+        link: "https://pavicouae.com/",
+    },
+    // You can add more items here if needed.
+];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "nandhakumarid023@gmail.com",
+    YOUR_FONE: "(+91)7448516897",
+    description: "Passionate Web Developer dedicated to delivering high-quality web solutions. Specializing in UI/UX design, website development, and mobile applications. Committed to continuous learning and staying updated with the latest industry trends. Let's connect and explore innovative ways to enhance user experiences! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/nandhakumar1998-23",
+    linkedin: "https://www.linkedin.com/in/nandhakumar-b-432024265/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
